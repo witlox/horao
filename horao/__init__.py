8105e0d6cc6acfd0ec0738ef86f6d1f51e5bcb23
Pim Witlox <pim@witlox.io>
2024-03-22 15:14:09 +0100

    old logging config removed

--- horao/__init__.py
+++ horao/__init__.py
@@ -5,11 +5,7 @@ import connexion
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 
-from basic.config.log_config import create_log
-
 ma = Marshmallow()
-logger = create_log()
-
 
 def init_api():
     app = connexion.App(__name__, specification_dir="./swagger/")
